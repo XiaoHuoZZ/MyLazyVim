054001241ddf81255e2f686e9ea666a67912f6aa
fengxing <mrfengx@163.com>
2024-08-01 22:56:33 +0800

    add project pattern

--- lua/plugins/project.lua
+++ lua/plugins/project.lua
@@ -1,7 +1,19 @@
 return {
     "ahmedkhalf/project.nvim",
     config = function()
-        require("project_nvim").setup({})
+        require("project_nvim").setup({
+            patterns = {
+                ".git",
+                "_darcs",
+                ".hg",
+                ".bzr",
+                ".svn",
+                "Makefile",
+                "package.json",
+                ".vscode",
+                ".idea",
+            },
+        })
         require("telescope").load_extension("projects")
         vim.keymap.set("n", "<leader>fp", "<cmd>Telescope projects<cr>", { desc = "Find Projects" })
     end,
