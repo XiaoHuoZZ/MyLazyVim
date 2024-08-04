6a3f39c82266fc7fa9bfd2ef0518e6451d07111d
fengxing <mrfengx@163.com>
2024-08-04 16:38:57 +0800

    add toggleTerm

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,12 @@
+return {
+    {
+        "akinsho/toggleterm.nvim",
+        version = "*",
+        opts = {--[[ things you want to change go here]]
+            open_mapping = [[<c-\>]],
+            start_in_insert = true,
+            terminal_mappings = true,
+            persist_mode = false,
+        },
+    },
+}
